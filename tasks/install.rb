b694307c0b01d60b923eff4434ddea05bf712023
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-12-06 19:51:12 -0800

    [dm-more] Reorganized Rakefile and rake tasks
    * Moved common spec and installation tasks into task/ subfolder within
      each plugin.
    * Rakefile now includes everything in it's task dir, and the dm-more
      task dir.
    * Updated trimmed-down Rakefile to be standarardized as much as possible
    * Updated specs to use local versions of gems when possible.  This
      means dm-more's rake spec can run prior to installing any dm-more
      gems.
    * Removed note in TODO about submitting TODO tickets to Lighthouse
    * Obfuscated email addresses using similar notation in all Rakefiles

--- tasks/install.rb
+++ tasks/install.rb
@@ -1,20 +1,19 @@
-WIN32 = (RUBY_PLATFORM =~ /win32|mingw|bccwin|cygwin/) rescue nil
-SUDO = WIN32 ? '' : ('sudo' unless ENV['SUDOLESS'])
+WIN32 = Gem.win_platform?
+SUDO  = WIN32 ? '' : ('sudo' unless ENV['SUDOLESS'])
 
-desc "Install #{GEM_NAME}"
-if WIN32
-  task :install => :gem do
-    system %{gem install --no-rdoc --no-ri -l pkg/#{GEM_NAME}-#{GEM_VERSION}.gem}
-  end
-  namespace :dev do
-    desc 'Install for development (for windows)'
-    task :winstall => :gem do
-      warn "You can now call 'rake install' instead of 'rake dev:winstall'."
-      system %{gem install --no-rdoc --no-ri -l pkg/#{GEM_NAME}-#{GEM_VERSION}.gem}
-    end
-  end
-else
-  task :install => :package do
-    sh %{#{SUDO} gem install --local pkg/#{GEM_NAME}-#{GEM_VERSION}.gem}
+desc "Install #{GEM_NAME} #{GEM_VERSION} (default ruby)"
+task :install => [ :package ] do
+  sh "#{SUDO} gem install --local pkg/#{GEM_NAME}-#{GEM_VERSION} --no-update-sources", :verbose => false
+end
+
+desc "Uninstall #{GEM_NAME} #{GEM_VERSION} (default ruby)"
+task :uninstall => [ :clobber ] do
+  sh "#{SUDO} gem uninstall #{GEM_NAME} -v#{GEM_VERSION} -I -x", :verbose => false
+end
+
+namespace :jruby do
+  desc "Install #{GEM_NAME} #{GEM_VERSION} with JRuby"
+  task :install => [ :package ] do
+    sh %{#{SUDO} jruby -S gem install --local pkg/#{GEM_NAME}-#{GEM_VERSION} --no-update-sources}, :verbose => false
   end
 end
