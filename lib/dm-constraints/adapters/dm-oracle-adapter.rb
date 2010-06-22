804d9bd1305ab94453ba5d48be041319df77b22e
Daniel Neighman <has.sox@gmail.com>
2010-06-23 09:30:55 +1000

    Updaets the oracle adapter with the correct sql
    [#1298]

--- lib/dm-constraints/adapters/dm-oracle-adapter.rb
+++ lib/dm-constraints/adapters/dm-oracle-adapter.rb
@@ -6,8 +6,47 @@ module DataMapper
 
       module OracleAdapter
         include DataObjectsAdapter
-      end
 
+        # oracle does not provide the information_schema table
+        # To question intenal state like postgres or mysql
+        # @see DataMapper::Constraints::Adapters::DataObjectsAdapter
+        # @api private
+        def constraint_exists?(storage_name, constraint_name)
+          statement = <<-SQL.compress_lines
+            SELECT COUNT(*)
+            FROM USER_CONSTRAINTS
+            WHERE table_name = ?
+            AND constraint_name = ?
+          SQL
+
+          select(statement, storage_name, constraint_name).first > 0
+        end
+
+
+        # @see DataMapper::Constraints::Adapters::DataObjectsAdapter#create_constraints_statement
+        def create_constraints_statement(storage_name, constraint_name, constraint_type, foreign_keys, reference_storage_name, reference_keys)
+          <<-SQL.compress_lines
+            ALTER TABLE #{quote_name(storage_name)}
+            ADD CONSTRAINT #{quote_name(constraint_name)}
+            FOREIGN KEY (#{foreign_keys.join(', ')})
+            REFERENCES #{quote_name(reference_storage_name)} (#{reference_keys.join(', ')})
+            INITIALLY DEFERRED DEFERRABLE
+          SQL
+        end
+
+        def destroy_constraints_statement(storage_name, constraint_name)
+          <<-SQL.compress_lines
+            ALTER TABLE #{quote_name(storage_name)}
+            DROP CONSTRAINT #{quote_name(constraint_name)}
+            CASCADE
+          SQL
+        end
+
+        def drop_table_statement(model)
+          table_name = quote_name(model.storage_name(name))
+          "DROP TABLE #{table_name} CASCADE_CONSTRAINTS"
+        end
+      end
     end
   end
 end
